c02a4bcf3e491cbdaff85a3292c2ff78d9d1d994
Shreeya Chauhan <97623578+shreeyachauhan@users.noreply.github.com>
2022-01-12 23:07:30 +0530

    Add files via upload

---
+++ ForLoop.py
@@ -0,0 +1,16 @@
+for i in range(10):
+    print(i,end=" ")
+print()
+    
+for i in range(3,10):
+    print(i,end=" ")
+print()
+
+for i in range(1,10,2):
+    print(i,end=" ")
+print()
+
+for i in range(10,0,-1):
+    print(i,end=" ")
+print()
+ 
